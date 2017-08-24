a3248fae0d2e391718982a96751173f807393bd3
ying-zhao <yingzhao1115@gmail.com>
2017-08-25 07:04:09 +0800

    bug fix

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -73,33 +73,6 @@ function sendMessageAi(event){
     apiai.end();
 }
 
-router.get('/index', function (req, res, next) {
-    console.log("index");
-    res.render('index', {title: 'Express'});
-}).post('/index',function (req, res, next) {
-    res.render('index',{title:'POST'})
-});
-
-router.post('/weatherWebhook',function(req,res,next){
-    let city = req.body.result.parameters['geo-city']; // city is a required param
-    // Get the date for the weather forecast (if present)
-    let date = '';
-    if (req.body.result.parameters['date']) {
-        date = req.body.result.parameters['date'];
-        console.log('Date: ' + date);
-    }
-    // Call the weather API
-    callWeatherApi(city, date).then((output) => {
-        // Return the results of the weather API to API.AI
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
-    }).catch((error) => {
-        // If there is an error let the user know
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
-    });
-}.bind(this));
-
 router.post('/botDemo',function(req,res,next){
     let intentName = req.body.result["metadata"]["intentName"];
     let date = '';
@@ -196,56 +169,4 @@ function getNameById(result,userid){
         }
     });
 }
-
-
-function callWeatherApi (city, date) {
-    return new Promise((resolve, reject) => {
-        // Create the path for the HTTP request to get the weather
-        let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-            '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-        console.log('API Request: ' + host + path);
-        // Make the HTTP request to get the weather
-        http.get({host: host, path: path}, (res) => {
-            let body = ''; // var to store the response chunks
-            res.on('data', (d) => { body += d; }); // store each response chunk
-            res.on('end', () => {
-                // After all the data has been received parse the JSON for desired data
-                let response = JSON.parse(body);
-                let forecast = response['data']['weather'][0];
-                let location = response['data']['request'][0];
-                let conditions = response['data']['current_condition'][0];
-                let currentConditions = conditions['weatherDesc'][0]['value'];
-                // Create response
-                let output = `Current conditions in the ${location['type']} 
-        ${location['query']} are ${currentConditions} with a projected high of
-        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-        ${forecast['date']}.`;
-                // Resolve the promise with the output text
-                console.log(output);
-                resolve(output);
-            });
-            res.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-}
-
-
-selectID = function (key) {
-    return new Promise((resolve,reject)=>{
-        MongoClient.connect(DB_CONN_STR,function (err,db) {
-            db.collection('id').find({id : key.toString() }).toArray(function (err,result) {
-                if(!err){
-                    db.close();
-                    resolve (result);
-                }else{
-                    console.log('Error:'+err);
-                    reject (err);
-                }
-            });
-        });
-    })
-};
 module.exports = router;
